4024c7791c0983da2d890ad998d6fa91a2d696e7
Surenthar Rajamohan <surenthar.rajamohan@gmail.com>
2022-12-04 18:36:21 +0800

    Basic Registration and login done

---
+++ app/src/main/java/com/blooddonationapp/startactivity/LoginActivity.java
@@ -0,0 +1,110 @@
+package com.blooddonationapp.startactivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //creating variables for each element in the login page
+
+        final EditText usernameInput = findViewById(R.id.LoginPage_ET_username);
+        final EditText passwordInput = findViewById(R.id.LoginPage_ET_password);
+        final Button loginButton = findViewById(R.id.LoginPage_BTN_login);
+        final TextView registerNow = findViewById(R.id.LoginPage_TV_registerNow);
+
+
+        //firebase variables (database)
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://blood-donation-applicati-79711-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+
+
+
+        //onClickListeners
+
+        //1) Login Button
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+
+                //exceptions
+                if(username.isEmpty())
+                    Toast.makeText(LoginActivity.this, "Please enter your username", Toast.LENGTH_SHORT).show();
+                else if(password.isEmpty())
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                else{
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            //check if this username exist
+                            if(snapshot.hasChild(username)){
+                                //username present
+                                //check if password is right
+                                final String databasePassword = snapshot.child(username).child("Password").getValue(String.class);
+
+                                if(databasePassword.equals(password)){
+                                    Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                }
+
+                                else{
+                                    Toast.makeText(LoginActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            else{
+                                Toast.makeText(LoginActivity.this, "Wrong username", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+        //2) Register Now (Button)
+
+        registerNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/blooddonationapp/startactivity/RegisterActivity.java
@@ -0,0 +1,111 @@
+package com.blooddonationapp.startactivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //creating variables for each element in the login page
+
+        final EditText emailInput = findViewById(R.id.RegisterPage_ET_emailAddress);
+        final EditText usernameInput = findViewById(R.id.RegisterPage_ET_username);
+        final EditText passwordInput = findViewById(R.id.RegisterPage_ET_password);
+        final EditText confirmPasswordInput = findViewById(R.id.RegisterPage_ET_confirmPassword);
+        final Button registerButton = findViewById(R.id.RegisterPage_BTN_register);
+        final TextView loginNow = findViewById(R.id.RegisterPage_TV_loginNow);
+
+        //firebase variables (database)
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://blood-donation-applicati-79711-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+
+        //onClickListeners
+
+        //1) Register Button
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = emailInput.getText().toString();
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+                final String confirmPassword = confirmPasswordInput.getText().toString();
+
+                //exceptions
+                if(email.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Please enter your email address", Toast.LENGTH_SHORT).show();
+                else if(username.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Please enter your username", Toast.LENGTH_SHORT).show();
+                else if(password.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                else if(confirmPassword.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Please enter your password again", Toast.LENGTH_SHORT).show();
+                else if(!(password.equals(confirmPassword)))
+                    Toast.makeText(RegisterActivity.this, "Entered passwords are not matching", Toast.LENGTH_SHORT).show();
+
+                //write data to database
+                else{
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            //check for duplicate email address
+                            if(snapshot.hasChild(username))
+                                Toast.makeText(RegisterActivity.this, "This username has been taken", Toast.LENGTH_SHORT).show();
+                            else{
+                                databaseReference.child("users").child(username).child("Email").setValue(email);
+                                databaseReference.child("users").child(username).child("Password").setValue(password);
+
+                                Toast.makeText(RegisterActivity.this, "User has been registered successfully!", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
+
+                }
+
+
+            }
+        });
+
+        //2) Login Now (Button)
+
+        loginNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
