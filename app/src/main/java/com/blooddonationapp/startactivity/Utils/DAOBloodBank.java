b8467b3a5d73c9eb8adc4907bf2f25449ed44ef2
faiqahAzman <u2001400@siswa.um.edu.my>
2023-01-09 19:56:00 +0800

    - notification for emergency request donor
    - only donors will get notification for emergency donor requests

--- app/src/main/java/com/blooddonationapp/startactivity/Utils/DAOBloodBank.java
+++ app/src/main/java/com/blooddonationapp/startactivity/Utils/DAOBloodBank.java
@@ -1,6 +1,7 @@
 package com.blooddonationapp.startactivity.Utils;
 
 
+import com.blooddonationapp.startactivity.UserData.User;
 import com.blooddonationapp.startactivity.UserData.bloodBank;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
@@ -10,11 +11,13 @@ import com.google.firebase.database.Query;
 // DAO IS DATA ACCESS OBJECT FOR CRUD OPERATIONS!!!!!!!! DONT ASK ME WHOEVER ASKED ME I WILL PUKUL
 public class DAOBloodBank {
     private DatabaseReference databaseReference;
+    private DatabaseReference userReference;
 
     public DAOBloodBank() {
         // TO INITIALIZE CONNECTION TO MY DATABASE
         FirebaseDatabase db = FirebaseDatabase.getInstance("https://blood-donation-applicati-79711-default-rtdb.asia-southeast1.firebasedatabase.app/");
         databaseReference = db.getReference(bloodBank.class.getSimpleName());
+        userReference = db.getReference("users");
     }
 
     public Task<Void> add(bloodBank bloodBankObject){
@@ -29,4 +32,10 @@ public class DAOBloodBank {
         return databaseReference
                 .orderByChild(childData);
     }
+
+    public Query getUser(){
+        return userReference.orderByKey();
+    }
+
+
 }
